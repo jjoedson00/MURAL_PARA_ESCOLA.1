6f21cc7573e6a1848545e618c1bccef153d0d990
jjoedson00 <jjoedson87@gmail.com>
2026-08-20 11:06:11 -0300

    fix: corrigidas rotas de extensao estática do node e sincronizada pagina admin com o banco

--- server.js
+++ server.js
@@ -1,28 +1,20 @@
 import express from 'express';
-import session from 'express-session';
 import path from 'path';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(session({
-    secret: 'chave-secreta-mural-escolar-2026',
-    resave: false,
-    saveUninitialized: false,
-    cookie: { secure: false }
-}));
 
-// Servir arquivos estáticos do front-end
-app.use(express.static('public'));
+// CORRIGIDO: Permite que o Express sirva os arquivos HTML sem que você precise digitar o ".html" na URL
+app.use(express.static('public', { extensions: ['html'] }));
 
-// Rota coringa para garantir o carregamento do painel
-app.get('*', (req, res) => {
+// Rota padrão para entregar a página inicial
+app.get('/', (req, res) => {
     res.sendFile(path.resolve('public', 'index.html'));
 });
 
-// CORRIGIDO: Injeção de porta dinâmica obrigatória para o Render aceitar conexões externas
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
-    console.log(`🚀 Servidor estático ativo na porta ${PORT}`);
+    console.log(`🚀 Sistema do Mural ativo localmente na porta ${PORT}`);
 });
